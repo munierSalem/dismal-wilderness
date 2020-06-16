22c32589a6949a6a13a66aaaf2593fca6416b0a4
Munier Salem <salem.munier@gmail.com>
2020-06-15 23:58:07 -0400

    added timeseries chart

--- d3-graphics.js
+++ d3-graphics.js
@@ -311,3 +311,170 @@ function d3_barchart(chart_ID,x,y_vars,data_path,tooltip_text,y_label){
   } // end isArray if/else
 
 } // end d3_scatter function
+
+
+function d3_linechart(chart_ID,x,y_vars,data_path,tooltip_text,x_label,y_label){
+
+  // infer y_label from y_vars if only one y_var given
+  if( !Array.isArray(y_vars) && typeof y_label === "undefined"){
+    y_label = y_vars;
+  } // end y_vars if
+  if(typeof x_label === "undefined"){
+    x_label = x;
+  } // end x_label if
+
+  // set the dimensions and margins of the graph
+  var margin = {
+    top:    10, 
+    right:  10, 
+    bottom: 40, 
+    left:   (typeof y_label !== "undefined") ? 60 : 40
+  },
+      width = 700 - margin.left - margin.right,
+      height = 350 - margin.top - margin.bottom;
+
+  chart = d3.select(chart_ID)
+    .classed("d3-linechart d3-chart",true);
+
+  // append the svg object to the body of the page
+  var svg = chart
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+  // y axis-label
+  if(typeof y_label !== "undefined"){
+    svg.append("text")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left+20)
+        .attr("x", -margin.top)
+        .attr("class","axis-label")
+        .text(y_label);
+  } // end x_label if
+  svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height + margin.top + 20)
+    .attr("class","axis-label")
+    .text(x_label);
+
+  if(typeof tooltip_text !== "undefined"){
+      // Define the div for the tooltip
+      var tooltip = d3.select("body").append("div") 
+          .attr("class", "d3-tooltip")
+          .style("opacity", 0);
+  }// end tooltip if
+
+  // Initialize the X axis
+  var x_scale = d3.scaleLinear()
+    .range([ 0, width ]);
+  var xAxis = svg.append("g")
+    .attr("transform", "translate(0," + height + ")");
+
+  // Initialize the Y axis
+  var y_scale = d3.scaleLinear()
+    .range([ height, 0]);
+  var y_axis = svg.append("g");
+
+  d3.csv(data_path,function(data){
+
+    // Reformat the data: we need an array of arrays of {x, y} tuples
+    var dataReady = y_vars.map( function(grpName) {
+      return {
+        name: grpName,
+        values: data.map(function(d) {
+          return {x: +d[x], y: +d[grpName], name: grpName};
+        })
+      };
+    });
+
+    // Update the X axis
+    var x_min = d3.min(data,d=>+d[x]), x_max = d3.max(data,d=>+d[x]);
+    x_scale.domain([x_min,x_max + (x_max-x_min)*0.1])
+    xAxis
+      .call(d3.axisBottom(x_scale).tickSize(-height*1.3).ticks(4).tickFormat(d3.format("d")))
+      .select("domain").remove();
+
+    // Update the Y axis
+    y_scale.domain([
+      d3.min(y_vars.map(y=>d3.min(data,d=>d[y]))),
+      d3.max(y_vars.map(y=>d3.max(data,d=>d[y])))
+    ]); // end y_scale.domain
+    y_axis
+      .call(d3.axisLeft(y_scale).tickSize(-width*1.3).ticks(4).tickFormat(d3.format(".0%")))
+      .select("domain").remove();
+
+    // A color scale: one color for each group
+    var myColor = d3.scaleOrdinal()
+      .domain(y_vars).range(['#cc2c00','#00A0CC','#9CD10C','#9860E7','#F8AD07']);
+
+    // Add the lines
+    var line = d3.line().x(d=>x_scale(d.x)).y(d=>y_scale(d.y))
+    svg.selectAll("myLines")
+      .data(dataReady)
+      .enter()
+      .append("path")
+        .classed("line",true)
+        .attr("d", d=>line(d.values))
+        .attr("stroke",d=>myColor(d.name))
+        .style("fill", "none");
+
+    // Add the markets
+    var point_collections = svg
+      // First we need to enter in a group
+      .selectAll("myDots")
+      .data(dataReady)
+      .enter()
+        .append('g')
+        .style("fill", d=>myColor(d.name))
+      // Second we need to enter in the 'values' part of this group
+      
+    var points = point_collections.selectAll("myPoints")
+      .data(function(d){ return d.values })
+      .enter()
+      .append("circle")
+        .classed('marker',true)
+        .attr("cx",d=>x_scale(d.x))
+        .attr("cy",d=>y_scale(d.y));
+
+
+    // add labels after ea. line
+    svg
+      .selectAll("myLabels")
+      .data(dataReady)
+      .enter()
+        .append('g')
+        .append("text")
+          .classed('line-label',true)
+          .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
+          .attr("transform", function(d) { 
+            return "translate(" + x_scale(d.value.x) + "," + y_scale(d.value.y) + ")"; })
+          .attr("x", 12) // shift the text a bit more right
+          .text(function(d) { return d.name; })
+          .style("fill", function(d){ return myColor(d.name) });
+
+
+    if(typeof tooltip_text !== "undefined"){
+        // define mouseover behavior for tooltips
+        points.on("mouseover", function(d) {
+            tooltip.transition()    
+                   .duration(200)   
+                   .style("opacity", .9);   
+            tooltip.html(tooltip_text(d)) 
+                   .style("left", (d3.event.pageX + 20) + "px")   
+                   .style("top", (d3.event.pageY - 28) + "px"); 
+        }) // end mouseover
+        .on("mouseout", function(d) {   
+              tooltip.transition()    
+                  .duration(500)    
+                  .style("opacity", 0); 
+        });// end mouseout
+    } // end tooltip if
+
+    }); // end d3.csv
+
+} // end d3_linechart function
