ac014c31b43fd49041b765bbf2a03f5e9e13c976
Munier Salem <salem.munier@gmail.com>
2020-07-21 00:53:55 -0400

    barchart additions

--- d3-graphics.js
+++ d3-graphics.js
@@ -128,7 +128,7 @@ function d3_scatter(chart_ID,x,y,data_path,tooltip_text,x_label,y_label){
 } // end d3_scatter
 
 
-function d3_barchart(chart_ID,x,y_vars,data_path,tooltip_text,y_label,sort,limit){
+function d3_barchart(chart_ID,x,y_vars,data_path,tooltip_text,y_label,sort,limit,x_label){
   /*
     Produce a horizontal bar chartacross the width of the page.  Optionally,
     pass a custom tool-tip text generator.  Pass in a *list* of y_vars.  If 
@@ -172,7 +172,7 @@ function d3_barchart(chart_ID,x,y_vars,data_path,tooltip_text,y_label,sort,limit
   var margin = {
     top:    10, 
     right:  10, 
-    bottom: 40, 
+    bottom: (typeof x_label !== "undefined") ? 60 : 40, 
     left:   (typeof y_label !== "undefined") ? 60 : 40
   },
       width = 700 - margin.left - margin.right,
@@ -199,7 +199,17 @@ function d3_barchart(chart_ID,x,y_vars,data_path,tooltip_text,y_label,sort,limit
           .attr("x", -margin.top)
           .attr("class","axis-label")
           .text(y_label);
-  } // end x_label if
+  } // end y_label if
+
+  // x axis-label
+  if(typeof x_label !== "undefined"){
+      svg.append("text")
+          .attr("text-anchor", "end")
+          .attr("x", width)
+          .attr("y", height + margin.top + 25)
+          .attr("class","axis-label")
+          .text(x_label);
+  } // end y_label if
 
   if(typeof tooltip_text !== "undefined"){
       // Define the div for the tooltip
@@ -502,3 +512,118 @@ function d3_linechart(chart_ID,x,y_vars,data_path,tooltip_text,x_label,y_label,l
     }); // end d3.csv
 
 } // end d3_linechart function
+
+
+function d3_geo_scatter(chart_ID,x,y,color,data_path,outline_path,tooltip_text){
+  /*
+    Produce a 1-d scatter plot across the width of the page.  Optionally,
+    pass a custom tool-tip text generator.
+
+    TODO: 
+      - axis limits currently hard-coded to POC chart, FIXME
+        - num axis ticks also hard-coded to POC chart, FIXME
+      - no support for dates
+      - width hard-coded to 700 pixels ... FIXME
+
+    Args:
+      chart_ID : required div ID (e.g. "#mychart")
+      x : x-variable (e.g. "dinosaurs")
+      y : y-variable (e.g. "mammals")
+      data_path : full URL to data file (currently only CSV)
+
+    Opt Args:
+      tooltip_text (undefined) : function that takes data row and
+        returns text/HTML to display within a tooltip.  If none
+        supplied, no tooltips are generated.
+
+    Returns:
+      undefined
+  */
+  
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 30, bottom: 40, left: 60},
+      width = 700 - margin.left - margin.right,
+      height = 700 - margin.top - margin.bottom;
+
+  // append the svg object to the body of the page
+  var Svg = d3.select(chart_ID)
+    .attr("class","d3-scatter")
+    .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+
+    // A color scale: one color for each group
+  var myColor = d3.scaleOrdinal()
+    .domain(['False','True']).range(['#cc2c00','#00A0CC','#7FD629','#F8AD07','#666666',
+      '#EE52A0','#9979BE']);
+
+  if(typeof tooltip_text !== "undefined"){
+
+    // Define the div for the tooltip
+    var tooltip = d3.select("body").append("div") 
+        .attr("class", "d3-tooltip")        
+        .style("opacity", 0);
+
+  }// end tooltip if
+
+
+  // // start with the outline
+  d3.json(outline_path,function(json){
+    console.log(json.features[4])
+  }); // end outline json
+
+
+  //Read the data
+  d3.csv(data_path, function(data) {
+
+    // Add X axis
+    var x_scale = d3.scaleLinear()
+      .domain([d3.min(data,d=>+d[x]), d3.max(data,d=>+d[x])])
+      .range([ 0, width ])
+    
+
+    // Add Y axis
+    var y_scale = d3.scaleLinear()
+      .domain([d3.min(data,d=>+d[y]), d3.max(data,d=>+d[y])])
+      .range([ height, 0])
+      .nice()
+
+    // Add dots
+    var data_points = Svg.append('g')
+      .selectAll("dot")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("cx", d=>x_scale(d[x]))
+        .attr("cy", d=>y_scale(d[y]))
+        .attr("r", 8)
+        .style("fill",d=>myColor(d[color]))
+        .style("stroke",d=>myColor(d[color]))
+        .attr("class","data-point");
+
+    if(typeof tooltip_text !== "undefined"){
+
+        // define mouseover behavior for tooltips
+        data_points.on("mouseover", function(d) {   
+              tooltip.transition()    
+                  .duration(200)    
+                  .style("opacity", .9);    
+              tooltip.html(tooltip_text(d)) 
+                  .style("left", (d3.event.pageX + 20) + "px")    
+                  .style("top", (d3.event.pageY - 28) + "px");  
+              }) // end mouseover
+        .on("mouseout", function(d) {   
+              tooltip.transition()    
+                  .duration(500)    
+                  .style("opacity", 0); 
+        });// end mouseout
+
+    } // end tooltip if
+
+  }); // end d3.csv
+  
+} // end d3_scatter
